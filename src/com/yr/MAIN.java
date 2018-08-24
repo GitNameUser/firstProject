b71e81368e06748a8cd3b5dd625abe4fca471c8d
GitNameUser <2922516139@qq.com>
2018-08-24 10:58:05 +0800

    测试2

--- src/com/yr/MAIN.java
+++ src/com/yr/MAIN.java
@@ -2,7 +2,8 @@ package com.yr;
 
 public class MAIN {
 	public static void main(String[] args) {
-		System.out.println(126);
+		System.out.println(55555);
+		System.out.println(1236);
 		System.out.println(1516);
 	}
 

eaac1adcd5104c66acc73c6d31cdfb0b2a1ab34a
GitNameUser <42628869+GitNameUser@users.noreply.github.com>
2018-08-24 11:18:07 +0800

    Update MAIN.java

--- src/com/yr/MAIN.java
+++ src/com/yr/MAIN.java
@@ -3,8 +3,8 @@ package com.yr;
 public class MAIN {
 	public static void main(String[] args) {
 		System.out.println(555555);
-		System.out.println(55555);
-		System.out.println(445555);
+		System.out.println(11);
+		System.out.println(112);
 		
 	}
 

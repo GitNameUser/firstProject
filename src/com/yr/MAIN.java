9186148506c5b180560793d302f0f98f84dd4db8
GitNameUser <2922516139@qq.com>
2018-08-24 11:17:31 +0800

    再一次测试

--- src/com/yr/MAIN.java
+++ src/com/yr/MAIN.java
@@ -2,9 +2,12 @@ package com.yr;
 
 public class MAIN {
 	public static void main(String[] args) {
+		System.out.println(123445);
+		System.out.println(113);
+		System.out.println(141);
 		System.out.println(555555);
-		System.out.println(55555);
-		System.out.println(445555);
+		System.out.println(11);
+		System.out.println(112);
 		
 	}
 
